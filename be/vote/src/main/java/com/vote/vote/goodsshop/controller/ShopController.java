84c0135ec8aa3f853ff2b0a95ef7e46369caf859
cockyb <cockyb@nate.com>
2020-05-22 01:54:48 +0900

    prdDB
    asdf

--- be/vote/src/main/java/com/vote/vote/goodsshop/controller/ShopController.java
+++ be/vote/src/main/java/com/vote/vote/goodsshop/controller/ShopController.java
@@ -1,14 +1,23 @@
 package com.vote.vote.goodsshop.controller;
 
+import com.vote.vote.repository.PrdJpaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ShopController {
 
+	@Autowired
+	PrdJpaRepository prdRepository;
 	@RequestMapping("/shop/index")
-	public String index() {
+	public String index(Model model) {
+
+		// model.addAttribute("prdList", prdRepository.findAll());
 		return "shop/index";
+
 	}
 
 	@RequestMapping("/shop/cart")

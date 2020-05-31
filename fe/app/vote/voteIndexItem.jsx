229591874914dd4dcc21c619250414d3d44a5319
choiminseok <mrmin02@naver.com>
2020-05-31 17:20:42 +0900

    블록체인 뷰, 데이터

--- fe/app/vote/voteIndexItem.jsx
+++ fe/app/vote/voteIndexItem.jsx
@@ -1,6 +1,6 @@
 import React, {Component}from 'react'
 import ReactDOM from 'react-dom';
-import ItemCard3 from '../items/ItemCard3.jsx';
+import ItemCard3 from '../items/itemCard3_big.jsx';
 import './voteIndex.css';
 import { Pagination } from '@material-ui/lab';
 
@@ -28,14 +28,14 @@ class VoteIndex extends Component {
     // }
     render() {
         const { data } = this.props;
-        console.log(this.props);
+        // console.log(this.props);
         return data.map((vote,index)=>{
             return (
                 <div key={'div'+index} className="vote_index_item">
                     <a href={"/vote/"+vote.id}>
                         <ItemCard3 img={vote.thumbnail} title={vote.title}/>
                     </a>
-                    <a href={'/vote/result/'+vote.id}>실시간 투표 결과</a>
+                    {/* <a href={'/vote/result/'+vote.id}>실시간 투표 결과</a> */}
                 </div>
             )
         })

30bbb9facbf48592e4c6e13417c618e392e6e43d
차재 <aaed1234@naver.com>
2020-05-31 21:53:33 +0900

    재영기본베이스 21:50

---
+++ be/vote/src/main/java/com/vote/vote/db/dto/Vote.java
@@ -0,0 +1,106 @@
+package com.vote.vote.db.dto;
+
+import java.util.Date;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.SequenceGenerator;
+
+
+@Entity
+@Table(name="vote")//name="vote" 생략가능
+public class Vote{
+    
+    @Id
+    @Column(name="voteid", nullable=false)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="VOTE_SEQ_GENERATOR")
+    @SequenceGenerator(name="VOTE_SEQ_GENERATOR", sequenceName="VOTE_SEQ", allocationSize = 1)
+    private int id;
+
+    @Column(nullable=false)
+    private String title;
+
+    @Column(nullable=false)
+    private String writer;
+
+    @Column(nullable=true)
+    private String address;
+
+    @Column
+    private int count;
+
+    @Column(name="starttime", nullable=true)
+    private String startTime;
+    @Column(name="endTime", nullable=true)
+    private String endTime;
+
+    @Column
+    private String thumbNail;
+
+    // @Column(nullable = true)
+    // private int program_id;
+
+
+    public int getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public String getWriter(){
+        return writer;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public int getCount(){
+        return count;
+    }
+    public String getStartTime(){
+        return startTime;
+    }
+    public String getEndTime(){
+        return endTime;
+    }
+    public String getThumbnail(){
+        return thumbNail;
+    }
+    // public int getProgram_id(){
+    //     return this.program_id;
+    // }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setWriter(String writer){
+        this.writer = writer;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public void setCount(int count){
+        this.count = count;
+    }
+    public void setStartTime(String startTime){
+        this.startTime = startTime;
+    }
+    public void setEndTIme(String endTime){
+        this.endTime = endTime;
+    }
+    public void setThumbnail(String thumbNail){
+        this.thumbNail = thumbNail;
+    }
+    // public void setProgram_id(int program_id){
+    //     this.program_id = program_id;
+    // }
+
+}
---
+++ fe/app/vote/voteShow.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import ItemCard from '../items/itemCard.jsx';
+import ItemCard2 from '../items/ItemCard2.jsx';
+import './voteShow.css'
+const axios = require('axios');
+
+var url = document.location.href;
+const num = url.split('/');
+var param = num[num.length-1];
+// url.substr(url.length-1,1);
+
+const regeneratorRuntime = require("regenerator-runtime");
+
+
+class VoteShow extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    
+    render() {
+        // {this.sendSelect.bind(this,index)}
+        return this.props.votes.map((vote,index)=>{
+            if (vote.name != 0){
+                return (
+                    <div key={vote.name+index} className="card_div" onClick={this.props.event.bind(this,index)}> 
+                        {/* <ItemCard key={vote.img} img={vote.img} name={vote.name} event={this.sendSelect.bind(this,index)}/>   */}
+                        <ItemCard2 key={vote.img} img={vote.img} name={vote.name}/>
+                    </div>
+                )
+            }
+        })
+    }
+}
+
+class Show extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = { votes: [], title: ""};
+        this.aa = "aaa";
+    }
+
+    async componentDidMount(){
+        let {data} = await axios.get('/vote/axios/'+param);
+        // console.log(data[0]);
+        this.setState({votes : data[0], title : data[1]});
+        // console.log(this.state);
+        
+    }
+
+    sendSelect(index){
+        const select  =  {"select" : index+1}
+        console.log(select);
+        if(!confirm("해당 후보에 투표하시겠습니까?")) return;
+        
+
+        axios.post('/vote/axios/'+param, select)
+        .then((response)=>{
+            if(response.data.errorMessage){
+                alert(response.data.errorMessage);
+                window.location.href="/vote";
+            }else{
+                alert(response.data.message);
+                window.location.href="/vote";
+            }
+        });
+        
+
+    }
+    render(){
+        const {title} = this.state.title
+        return(
+            <div>
+                <h2>투표</h2>
+                <div><a href="/vote">목록으로 가기</a></div>
+                <div><h3>{title}</h3></div>
+                <VoteShow votes={this.state.votes} event={this.sendSelect}/>                
+            </div>
+        )
+    }
+        
+}
+
+ReactDOM.render(<Show/>,document.getElementById('voteShow'));

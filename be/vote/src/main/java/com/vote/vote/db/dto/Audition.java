a0ca6764442343a1335ea1b4c356086af099b519
MinGyu <54360814+GGYURIN@users.noreply.github.com>
2020-05-31 17:26:33 +0900

    audition

---
+++ be/vote/src/main/java/com/vote/vote/db/dto/Audition.java
@@ -0,0 +1,141 @@
+package com.vote.vote.db.dto;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+@Table(name="audition")
+public class Audition {
+	
+	 	@Id
+	    @Column(nullable=false, name="audition_id")
+	    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="AUDITION_SEQ_GENERATOR")
+	    @SequenceGenerator(name="AUDITION_SEQ_GENERATOR", sequenceName="AUDITION_SEQ", allocationSize = 1)
+	    private int auditionid;
+
+	    @Column(nullable=true, name="a_title")
+	    private String atitle;
+
+	    @Column(nullable=true,name="a_content")
+	    private String acontent;
+	    
+	    @Column(nullable=true,name="a_category")
+	    private String acategory;
+
+	    @Temporal(TemporalType.TIMESTAMP)
+	    @Column(nullable=true,name="a_startdate")
+	    private Date astartdate;
+	    
+
+	    @Column(nullable=true,name="a_enddate")
+	    private Date aenddate;
+
+	    @Column(nullable=true,name="a_recruits")
+	    private Integer arecruits;
+
+	    @Column(nullable=true,name="a_issue")
+	    private Date aissue;
+
+	    @Column(nullable=true,name="program_id")
+	    private Integer programid;
+
+	    @Column(nullable=true,name="r_id")
+	    private Integer rid;
+
+		
+
+		public int getAuditionid() {
+			return auditionid;
+		}
+
+		public void setAuditionid(int auditionid) {
+			this.auditionid = auditionid;
+		}
+
+		public String getAtitle() {
+			return atitle;
+		}
+
+		public void setAtitle(String atitle) {
+			this.atitle = atitle;
+		}
+
+		public String getAcontent() {
+			return acontent;
+		}
+
+		public void setAcontent(String acontent) {
+			this.acontent = acontent;
+		}
+
+		public String getAcategory() {
+			return acategory;
+		}
+
+		public void setAcategory(String acategory) {
+			this.acategory = acategory;
+		}
+
+		public Date getAstartdate() {
+			return astartdate;
+		}
+
+		public void setAstartdate(Date astartdate) {
+			this.astartdate = astartdate;
+		}
+
+		public Date getAenddate() {
+			return aenddate;
+		}
+
+		public void setAenddate(Date aenddate) {
+			this.aenddate = aenddate;
+		}
+
+		public Integer getArecruits() {
+			return arecruits;
+		}
+
+		public void setArecruits(Integer arecruits) {
+			this.arecruits = arecruits;
+		}
+
+		public Date getAissue() {
+			return aissue;
+		}
+
+		public void setAissue(Date aissue) {
+			this.aissue = aissue;
+		}
+
+		public Integer getProgramid() {
+			return programid;
+		}
+
+		public void setProgramid(Integer programid) {
+			this.programid = programid;
+		}
+
+		public Integer getRid() {
+			return rid;
+		}
+
+		public void setRid(Integer rid) {
+			this.rid = rid;
+		}
+
+		
+		
+	    
+
+}
---
+++ be/vote/src/main/java/com/vote/vote/repository/AuditionJpaRepository.java
@@ -0,0 +1,18 @@
+package com.vote.vote.repository;
+
+import java.util.ArrayList;
+import java.io.Serializable;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vote.vote.db.dto.Audition;
+
+@Repository
+public interface AuditionJpaRepository  extends JpaRepository<Audition, String>{
+	public ArrayList<Audition> findAll();
+	public Audition findByAuditionid(int auditionid);
+
+}

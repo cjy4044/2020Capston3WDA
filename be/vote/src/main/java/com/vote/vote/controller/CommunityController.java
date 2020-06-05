8e7407512f8241ed099afd2fee2b2c27caba982b
차재 <aaed1234@naver.com>
2020-06-05 18:48:30 +0900

    ddd
    d

--- be/vote/src/main/java/com/vote/vote/controller/CommunityController.java
+++ be/vote/src/main/java/com/vote/vote/controller/CommunityController.java
@@ -1,15 +1,26 @@
 package com.vote.vote.controller;
 
 import java.security.Principal;
+import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.vote.vote.db.dto.Program;
+import com.vote.vote.repository.ProgramJpaRepository;
 
 @Controller
 @RequestMapping("/community")
 public class CommunityController {
 
+	@Autowired
+	ProgramJpaRepository programRepository;
+	
     @RequestMapping(value={"","/"})
 	public String index() {
 		// System.out.println("/ --> index");
@@ -19,6 +30,25 @@ public class CommunityController {
 		// }
 		return "community/index";
 	}	
+    
+    @ResponseBody
+	@RequestMapping(value={"/axios","/axios/"})
+	public JSONArray createAxios() {
+		
+		JSONArray result = new JSONArray();
+
+		List<Program> programList = programRepository.findAll();
+
+		for(Program program : programList){
+			JSONObject json = new JSONObject();
+			json.put("id", program.getId());
+			json.put("name",program.getName());
+			json.put("img",program.getImg());
+			result.add(json);
+		}
+		System.out.println("프로그램 json 넘김");
+		return result;
+	}
 	
 	
 }

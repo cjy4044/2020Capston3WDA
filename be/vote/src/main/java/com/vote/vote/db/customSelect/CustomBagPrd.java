9851f6c3e7f162e6f002494b4c3f5e95d016f0ac
choiminseok <mrmin02@naver.com>
2020-06-19 02:57:43 +0900

    쇼핑몰 쿼리 짜는중

--- be/vote/src/main/java/com/vote/vote/db/customSelect/CustomBagPrd.java
+++ be/vote/src/main/java/com/vote/vote/db/customSelect/CustomBagPrd.java
@@ -1,5 +1,14 @@
 package com.vote.vote.db.customSelect;
 
 public class CustomBagPrd {
-    
+ 
+// select * 
+// from mybag b, 
+//     (select p.product_id, p.p_name, p.image, o.option_id ,o.o_title, o.o_price, o.p_stock
+//     from product p , product_option o
+//     where p.product_id = o.product_id) g
+// where b.product_id = g.product_id
+// and b.option_id = g.option_id
+// and b.r_id = '261'
+// order by b.id desc
 }

5cedf40e3620478d55b957f8cd3608e44eedd5ae
YoungJJinKim <61184563+YoungJJinKim@users.noreply.github.com>
2020-06-18 17:13:38 +0900

    0618
    0618

--- be/vote/src/main/java/com/vote/vote/repository/RfileRepository.java
+++ be/vote/src/main/java/com/vote/vote/repository/RfileRepository.java
@@ -11,12 +11,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RfileRepository extends JpaRepository<Rfile, Integer> {
-    public ArrayList<Rfile> findByHotclibid(int hotclibid);
-    
+    public Rfile findByHotclibid(int hotclibid);
     public Rfile findByFilename(String filename);
-    
     public Rfile findByFileid(int fileid);
-    
     public List<Rfile> findByPid(int pid);
     
     
